e668b7821a1d801ed01a06ae4da9111895fa0332
Dwenn <Dwenn@live.fr>
2018-09-15 23:40:54 +0200

    Ajout de Categories, Langues , et resultats.js

---
+++ api/models/Resultat.js
@@ -0,0 +1,43 @@
+/**
+ * Utilisateurs.js
+ *
+ * A user who can log in to this application.
+ */
+
+module.exports = {
+    tableName:  'mots',
+    attributes: {
+  
+      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+  
+      numLangue: {
+        type: 'int',
+        required: true,
+      },
+
+      libelle: {
+        type: 'string',
+        required: true,
+        maxlenght : 50,
+      },
+  
+
+  
+      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+      // n/a
+      
+  
+      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+      // n/a
+  
+    },
+  
+  
+  };
+  
