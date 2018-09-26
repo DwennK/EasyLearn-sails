3967a5a13ccf5f3160261f28d4928ceb5133737c
Dwenn <Dwenn@live.fr>
2018-09-26 19:26:16 +0200

    Ajout d'une liste d'utilisateurs dans /game/play

--- api/controllers/game/view-play.js
+++ api/controllers/game/view-play.js
@@ -18,10 +18,25 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    // Respond with view.
-    return exits.success();
+    var things = [
+      { id:0 , label: 'Ceci est le label '},
+      { id:1 , label: 'Ceci est le label1'},
+      { id:2 , label: 'Ceci est le label2'},
+      { id:3 , label: 'Ceci est le label3'},
+    ];
 
-  }
+    var userxs = await Utilisateurs.find();
+
+    // Respond with view.
+    return exits.success(
+      {
+            //On y retourne le tableau de choses
+            things: things,
+            userxs: userxs
+      }
+    );
+
+  },
 
 
 };
--- assets/js/pages/game/play.page.js
+++ assets/js/pages/game/play.page.js
@@ -12,15 +12,24 @@ parasails.registerPage('play', {
   beforeMount: function() {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
+
   },
   mounted: async function() {
     //…
+
+    
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    //…
+
+  
+    clickThing: function(){
+      console.log(things);
+      console.log('clicked thing #');
+    }
+
   }
 });
